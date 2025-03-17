e4487a10e92fd4bc2b7bb3ff48510a4881a50758
jpelage <jeremy.pelage@outlook.com>
2025-03-17 18:26:01 +0100

    polariod__image

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,39 @@
+
 <script setup>
+    import { ref, computed } from 'vue';
     const props = defineProps(['projet'])
     const baseUrl = import.meta.env.BASE_URL;
+    // defineEmits(['close']);
+
+    const selectedImageIndex = ref(-1);
+
+    const images = computed(() => Array.isArray(props.projet.image) ? props.projet.image : []);
+
+    // Fonction pour afficher une image dans la modal__imagebis
+    const showImage = (ind) => {
+        console.log("showImage:", ind, "facteur", selectedImageIndex);
+        if (ind >= 0){
+            selectedImageIndex.value = ind;
+        }
+    };
+
+    // Fonction pour naviguer entre les images
+    const prevImage = () => {
+        if (selectedImageIndex.value > 0) {
+            selectedImageIndex.value--;
+        }
+    };
+
+    const nextImage = () => {
+        if (selectedImageIndex.value < props.projet.image.length - 1) {
+            selectedImageIndex.value++;
+        }
+    };
+
+    // Fonction pour fermer l'image agrandie
+    const closeImage = () => {
+        selectedImageIndex.value = -1; // null
+    };
 </script>
 
 <template>
@@ -24,7 +57,13 @@
             <div class="modal__images">
                 <h3 v-if="projet.image.length<2">Image</h3>
                 <h3 v-else>Images</h3>
-                <img v-for="i in projet.image" :src="`${baseUrl}${i.src}`" :alt="i.alt">
+                <img v-for="i in projet.image"
+                :key="0" 
+                :src="`${baseUrl}${i.src}`" 
+                :alt="i.alt"
+                @click="showImage(0)"
+                class="modal__thumbnail">
+                
             </div>
             <div class="modal__info">
                 <h3>Informations</h3>
@@ -39,7 +78,20 @@
             </div>
         </div>
     </div>
+    <!-- Modal pour afficher l'image en grand -->
+    <div v-if="selectedImageIndex !== -1" class="modal__imagebis" @click="closeImage">
+        <!-- <span class="close" @click="closeImage">&times;</span> -->
+        <div v-if="selectedImageIndex !== -1" class="modal__content" @click.stop>
+            <button v-if="selectedImageIndex > 0" class="prev" @click="prevImage">&#10094;</button>
+            <img v-if="images[selectedImageIndex]" :src="`${baseUrl}${images[selectedImageIndex].src}`" :alt="images[selectedImageIndex].alt" @click.stop>
+            <button v-if="selectedImageIndex < projet.image.length - 1" class="next" @click="nextImage">&#10095;</button>
+        </div>
+    </div>
 </template>
+<script>
+let 
+zoom
+</script>
 
 <style scoped>
     .modal__info {
@@ -48,6 +100,8 @@
 
     .modal__info__description {
         text-align: justify;
+        overflow-y: auto;
+        height: 100vh;
     }
 
     .modal__info h4 {
@@ -66,6 +120,79 @@
         flex-wrap: wrap;
         width: 50%;
         justify-content: center;
+        height: 100vh;
+        overflow-y: scroll;
+        scrollbar-color: #060640 transparent;
+        /* scrollbar-color: #060640; */
+        scrollbar-width: auto;    
+    }
+    /* Modal d'affichage de l'image sélectionnée */
+    .modal__imagebis {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.8);
+        /* display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column; */
+    }
+    .modal__content{
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 80%;
+        position: fixed;
+        z-index: 4;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin-inline: auto;
+    }
+
+    .modal__content img {
+        max-width: 80%;
+        max-height: 80vh;
+        border-radius: 5px;
+        width: fit-content !important;
+    }
+
+    /* Bouton de fermeture */
+    .close {
+        position: absolute;
+        top: 20px;
+        right: 30px;
+        /* font-size: 30px; */
+        font-size: 5.5em;
+        color: white;
+        cursor: pointer;
+    }
+
+    /* Boutons de navigation */
+    .prev, .next {
+        position: absolute;
+        top: 50%;
+        background: rgba(255, 255, 255, 0.5);
+        border: none;
+        font-size: 30px;
+        cursor: pointer;
+        padding: 10px;
+        transform: translateY(-50%);
+    }
+
+    .prev { left: 20px; }
+    .next { right: 20px; }
+
+    .prev:hover, .next:hover {
+        background: rgba(255, 255, 255, 0.8);
+    }
+    .modal__imagebis img{
+        width: 100%;
     }
 
     .modal__main {
